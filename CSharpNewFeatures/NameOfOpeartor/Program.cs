f3fd2e2db9d8f4d15ac8acadc16dbf0668684fea
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2014-12-26 01:59:24 +0530

    Removed unused namespaces

---
+++ CSharpNewFeatures/NameOfOpeartor/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace NameOfOpeartor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee
+                                { FirstName = "Jalpesh",
+                                LastName = "Vadgama" };
+            Print(null);
+        }
+        static void Print(Employee employee)
+        {
+            //Old way of doing it
+            //if (employee == null)
+            //    throw new ArgumentException("employee");
+
+            if (employee == null)
+                throw new ArgumentException(nameof(employee));
+
+            Console.WriteLine(employee.FirstName);
+            Console.WriteLine(employee.LastName);
+        }
+    }
+
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
