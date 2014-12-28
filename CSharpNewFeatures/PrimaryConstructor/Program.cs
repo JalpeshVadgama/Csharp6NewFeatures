325aa8b723d41b044d055a395c7c94bcade5bbfa
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2014-12-28 14:34:13 +0530

    Added Automatic property initialization console application.

---
+++ CSharpNewFeatures/PrimaryConstructor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimaryConstructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class Employee(string firstName)
+    {
+        private string _firstName=firstName;
+    private string _lastname;
+    }
+}
