76e743f1a4675f006f870e40de1fe550de1ab63e
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-01-01 23:42:51 +0530

    Added remaining program.cs file.

---
+++ CSharpNewFeatures/ExceptionFilters/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ExceptionFilters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                throw new Exception("Jalpesh");
+            }
+
+            //catch (Exception ex)
+            //{
+            //    if(ex.Message =="Jalpesh")
+            //        Console.WriteLine("Exception with Message Jalpesh is executed");
+            //    else
+            //        Console.WriteLine("Exception with Message DotNetJalps executed");
+            //}
+            catch (Exception ex) if (ex.Message == "Jalpesh")
+            {
+                Console.WriteLine("Exception with Message Jalpesh is executed");
+            }
+            catch (Exception ex) if (ex.Message == "DotNetJalps")
+            {
+                Console.WriteLine("Exception with Message DotNetJalps executed");
+            }
+        }
+    }
+}
