21c3898b865cab9046e4c43bc10c7ef4d1edc8b8
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-01-03 15:10:48 +0530

    Added example of string interpolation

---
+++ CSharpNewFeatures/StringInterpolation/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace StringInterpolation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Jalpesh Vadgama";
+
+            //Old way of string concentaionation 
+            Console.WriteLine("My name is" + name);
+            Console.WriteLine("My name is {0}", name);
+
+            //New way of doing this
+            Console.WriteLine("My name is \{name}");
+        }
+    }
+}
