54e53730ccf825ae525b2506f5b6ad8d8cb61a40
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-01-08 00:27:06 +0530

    Added a new example of dictionary initializer

---
+++ CSharpNewFeatures/DictionaryInitializer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionaryInitializer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> Person = new Dictionary<int, string>()
+            {
+                [1] = "Jalpesh Vadgama",
+                [2] = "Vishal Vadgama"
+            };
+            foreach (var person in Person)
+            {
+                Console.WriteLine("Key:\{person.Key}\, Value=\{person.Value}\ ");
+            }
+        }
+    }
+}
